66fd2093b855176426d312c70dbc98d084ca3d3c
Abhinav Garg <abhinav.garg1218@gmail.com>
2018-07-14 12:43:38 +0530

    add render

--- app/models/multi_agent_service.rb
+++ app/models/multi_agent_service.rb
@@ -1,18 +1,28 @@
 class MultiAgentService < ApplicationRecord
+
+  validates :consumers_count, :prosumers_count, :agents_count, presence: true
+  before_save :calculate_time_consumed, :determine_grid_kind
   REQUEST_GENERATION_TIME = 10
   INTER_AGENT_PROCESSING_TIME = 5
   RESPONSE_SENDING_TIME = 10
 
-  validates :consumers_count, :prosumers_count, :agents_count, presence: true
-  before_save :calculate_time_consumed, :determine_grid_kind
+  def render_time
+    "(#{REQUEST_GENERATION_TIME} +
+     #{INTER_AGENT_PROCESSING_TIME} +
+     #{INTER_AGENT_PROCESSING_TIME * agents_count} +
+     #{RESPONSE_SENDING_TIME}) x (#{total_load})
+     = #{calculate_time_consumed}"
+  end
 
   private
 
+  def total_load
+    "#{consumers_count} + #{prosumers_count}"
+  end
+
   def calculate_time_consumed
-    consumer_count = consumers_count
-    prosumer_count = prosumers_count
     agent_count = agents_count + 2
-    total_load = (consumer_count + prosumer_count)
+    total_load = consumers_count + prosumers_count
     time_for_one_request = REQUEST_GENERATION_TIME + INTER_AGENT_PROCESSING_TIME * agent_count + RESPONSE_SENDING_TIME
     self.total_time_consumed = total_load * time_for_one_request
   end
